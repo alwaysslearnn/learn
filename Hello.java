b84f08086de5d8d3a322250c219aba3c492f8fc2
alwaysslearnn <alwaysslearnn@gmail.com>
2022-03-17 21:03:12 +0000

    Update Hello.java
    Added new message

--- Hello.java
+++ Hello.java
@@ -5,6 +5,7 @@ public class MyFirstJavaProgram {
     */
 
    public static void main(String []args) {
-      System.out.println("Hello World"); // prints Hello World
+      //System.out.println("Hello World"); // prints Hello World
+	        System.out.println("Happy Learning!");
    }
 }
